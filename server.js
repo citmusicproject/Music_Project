ba1a59a8daeb61f40eb8cd2c7bdc4eb4c329e701
Alex Phan <Alexphan1024@gmail.com>
2018-04-23 22:06:10 -0700

    updated

--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser')
 const app = express();
 
-hbs.registerPartials(__dirname +'/views/partial');
+hbs.registerPartials(__dirname + '/views/partial');
 app.set('views', './views');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/',function(req,res){
-	res.render('about.hbs',{
-		title: "Top 100 Hits"
-		
-	});
+String.prototype.format = function() {
+    a = this;
+    for (k in arguments) {
+        a = a.replace("{" + k + "}", arguments[k])
+    }
+    return a
+}
+
+app.get('/', function(req, res) {
+    res.send('<a href="/search">Search</a>')
+});
+
+app.post('/', function(req, res) {
+    res.send("Song: {0}\nFavourite: {1}\nRating: {2}/5\n"
+        .format(req.body.song, req.body.favourite, req.body.rating)+"<a href='/'>Back</a>")
+    // console.log(req.body);
+});
+
+app.get('/search', function(req, res) {
+    res.render('search.hbs', {
+        title: "Search"
+    });
+});
+
+app.post('/rating', function(req, res) {
+    res.render('rating.hbs', {
+        title: "Rating",
+        data: req.body
+    });
+    // console.log(req.body);
 });
 
 app.listen(8080)
