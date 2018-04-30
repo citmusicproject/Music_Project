b743cf2d92bc68157ce01c07d366bc2f8ef0d97d
Name <+name@users.noreply.github.com>
2018-04-30 16:57:59 -0700

    minor fix for better reading

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const express = require('express');
 const hbs = require('hbs');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
-const login = require('./login.js')
-const alert = require('alert-node')
+const login = require('./login.js');
+const alert = require('alert-node');
 
 hbs.registerPartials(__dirname + '/views/partial');
 app.set('views', './views');
@@ -12,15 +12,15 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-String.prototype.format = function() {
+String.prototype.format = function () {
     a = this;
     for (k in arguments) {
-        a = a.replace("{" + k + "}", arguments[k])
+        a = a.replace("{" + k + "}", arguments[k]);
     }
-    return a
+    return a;
 }
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.hbs', {
         login: "Log in or Sign up",
         link: "login",
@@ -31,74 +31,78 @@ app.get('/', function(req, res) {
     });
 });
 
-app.post('/', function(req, res) {
-	var num = '/index'+req.body.acct
+app.post('/', function (req, res) {
+    var num = '/index' + req.body.acct;
     res.send("<br>Song: {0}</br><br>Favourite: {1}</br><br>Rating: {2}/5</br>"
-        .format(req.body.song, req.body.favourite == "on", req.body.rating) + `<button onclick="location.href = '/index'+req.body.acct ";>Back</button>`)
+        .format(req.body.song, req.body.favourite == "on", req.body.rating) +
+        `<button onclick="location.href = '/index'+req.body.acct ";>Back</button>`);
 });
 
-app.post('/rating', function(req, res) {
-	console.log(req.body);
+app.post('/rating', function (req, res) {
+    console.log(req.body);
     res.render('rating.hbs', {
         title: "Rating",
         data: req.body
     });
 });
 
-app.get('/login', function(req, res) {
+app.get('/login', function (req, res) {
     res.render('login.hbs');
 });
 
-app.post('/login', function(req, res) {
-    var userId = req.body.email
-    var userPw = req.body.pw
-    var login_info = login.loadDatabase()
+app.post('/login', function (req, res) {
+    var userId = req.body.email;
+    var userPw = req.body.pw;
+    var login_info = login.loadDatabase();
     for (i = 0; i < login_info.length; i++) {
         if (userId == login_info[i].email && userPw == login_info[i].pw) {
             var first_name = login_info[i].first;
             var last_name = login_info[i].last;
             // console.log(first_name);
             // alert("Login Success")
-            app.get('/index' + i.toString(), function(req, res) {
-                res.render('index.hbs', {
-                    login: `Hi, ${first_name} ${last_name} ${userId}`,
-                    home: "/index" + i.toString(),
-                    link: "",
-                    discover: "/discover",
-                    playlist: "/Playlist"+i.toString(),
-                    index: "1",
-                    acct: i
+            app.get('/index' + i.toString(),
+                function (req, res) {
+                    res.render('index.hbs',
+                        {
+                            login: `Hi, ${first_name} ${last_name} ${userId}`,
+                            home: "/index" + i.toString(),
+                            link: "",
+                            discover: "/discover",
+                            playlist: "/Playlist" + i.toString(),
+                            index: "1",
+                            acct: i
+                        });
                 });
-            });
-            app.get('/Playlist' + i.toString(), function(req, res) {
-                res.render('Playlist.hbs')
-            });
+            app.get('/Playlist' + i.toString(),
+                function (req, res) {
+                    res.render('Playlist.hbs');
+                });
             var valid = true
-            res.redirect('/index' + i.toString())
-            break
+            res.redirect('/index' + i.toString());
+            break;
         } else {
-            var valid = false
+            var valid = false;
         }
     }
     if (!valid) {
-        res.render('login.hbs')
-        alert("Login Failed")
+        res.render('login.hbs');
+        alert("Login Failed");
     }
-})
+});
 
 
-app.get('/signup', function(req, res) {
-    res.render('signup.hbs')
-})
+app.get('/signup', function (req, res) {
+    res.render('signup.hbs');
+});
 
-app.post('/signup', function(req, res) {
+app.post('/signup', function (req, res) {
     var id = req.body.email;
     var pw = req.body.pass;
     var fname = req.body.fname;
     var lname = req.body.lname;
     if (id == "" || pw == "" || fname == "" || lname == "") {
-        res.redirect('/signup')
-        alert('Missing Values')
+        res.redirect('/signup');
+        alert('Missing Values');
     } else {
         var user = {
             email: id,
@@ -106,15 +110,14 @@ app.post('/signup', function(req, res) {
             first: fname,
             last: lname
         }
-        var login_info = login.loadDatabase()
-        login_info.push(user)
-        var valid = login.addUser(login_info)
+        var login_info = login.loadDatabase();
+        login_info.push(user);
+        var valid = login.addUser(login_info);
         if (valid) {
-            alert('Successfully registered')
-            res.redirect('/login')
+            alert('Successfully registered');
+            res.redirect('/login');
         }
     }
-
-})
+});
 
 app.listen(8080)
