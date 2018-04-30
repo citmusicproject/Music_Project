fed2cb461e69b8cde4e37ab55440613792720452
jerrbare <jerry__luu@hotmail.com>
2018-04-29 23:00:47 -0700

    Search Function saves links in JSON file

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 var search = require('youtube-search');
 var keyWord = '';
 var secret = require('./keys');
-
+var fs = require('fs');
+var list = [];
 var password = secret.api_key;
 
  
 var opts = {
-  maxResults: 1,
-  key: password
+	maxResults: 5, 
+	key: password
 };
+
+function searchYoutube(keyword) {
+	search(keyword, opts, function(err, results) {
+		if(err) return console.log(err);
+	 
+
+		var i;
+		for (i = 0; i < results.length; i++) { 
+			list.push(results[i].link);
+
+		}
+		
+		
+	 	var test = JSON.stringify(list);
+	  	fs.writeFile('test.JSON', test);
+	})
+}
  
-search('jsconf', opts, function(err, results) {
-  if(err) return console.log(err);
- 
-  console.dir(results);
-});
+function readJSON () {
+	fs.readFile('test.JSON',test);
+	var test = JSON.parse(test);
+}
+
+module.exports = {
+	readJSON,
+	searchYoutube
+};
