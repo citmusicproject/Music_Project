ca15ea5dff7790c50442f0544c681dd790a47cb4
github_sjmichaelp <sjmichaelp@gmail.com>
2018-05-04 14:48:44 -0700

    update

--- searchyoutube.js
+++ searchyoutube.js
@@ -3,55 +3,25 @@ var keyWord = '';
 // var secret = require('./keys');
 var fs = require('fs');
 var list = [];
-var password = "";
+var password = '';
+
+
 var opts = {
     maxResults: 5,
     key: password
 };
 
-fs.readFile('keys', (er, da) => {
-    password = da.toString().substring(1);
-    opts = {
-        maxResults: 5,
-        key: password
-    }
-});
-
-function gpassword() {
-    return password;
-}
-
 function searchYoutube(keyword, callback) {
-    search(keyword, opts, function (err, results) {
+    search(keyword, opts, function(err, results) {
         if (err) {
             console.log(err);
         } else {
-
-        	console.log(results);
         	var i = 0;
-        	var list = [];
-        	console.log(results[0].link);
-     
-        	
-        	for (var i = 0; i < results.length; i++) {
-        		if (results[i].link.includes('/channel/')) {
-        			continue;
-        		}else {
-
-        			list.push(results[i].link.split('=')[1]);
-        			
-
-        		}
-
-        		console.log(list);
-        	}
-        	
-        	
             callback(undefined, {
-                link: list[0],
+                link: results[0].link.split('=')[1],
                 thumbnails : results[0].thumbnails.default.url,
                 title: results[0].title
-            });
+            })
         }
         // var i;
         // for (i = 0; i < results.length; i++) {
@@ -60,6 +30,7 @@ function searchYoutube(keyword, callback) {
         // }
         // var test = JSON.stringify(list);
         // fs.writeFile('test.JSON', test);
+
     });
 };
 
@@ -70,8 +41,7 @@ function readJSON() {
 
 module.exports = {
     readJSON,
-    searchYoutube,
-    gpassword
+    searchYoutube
 };
 
 // searchYoutube("Drake", (errorMessage, results) => {
