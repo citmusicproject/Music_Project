0031f6e8d0a78330c7fbb96152086d07c884d940
jyk926 <brikim92@gmail.com>
2018-05-14 19:58:37 -0700

    Add generating unique random ID

--- login.js
+++ login.js
@@ -1,8 +1,14 @@
 const key = require('./dbkeys.js') //File that stores database credentials
 var mysql = require('mysql'); //mysql module
 const bcrypt = require('bcrypt'); // used to encrypt passwords
-// //create connection with MySQL
-const alert = require('alert-node')
+const alert = require('alert-node');
+
+//random unique id generator
+var uniqueID = function () {
+    return Math.random().toString(36).substr(2, 8);
+  };
+
+//create connection with MySQL
 var connection = mysql.createConnection({
     host: key.RDS_HOSTNAME,
     user: key.RDS_USERNAME,
@@ -11,66 +17,6 @@ var connection = mysql.createConnection({
     database: key.RDS_DB_NAME
 });
 
-<<<<<<< HEAD
-function register(user){
-  connection.connect(function(err) {
-    if (err) { //if database fail connecting
-      console.error('Database connection failed: ' + err.stack);
-      return;
-    } //if database connected
-    console.log('Connected to database.');
-  });
-
-  console.log('user data', user)
-  const today = new Date();
-  const users = {
-      "first_name":user.first,
-      "last_name":user.last,
-      "email":user.email,
-      "password":user.pw,
-      "created":today,
-      "modified":today
-  }
-  
-  connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-    if (error) {
-      console.log("error ocurred",error);
-    }else{
-      console.log('Result: ', results);
-    }
-  });
-  connection.end()
-} 
-
-function login(user){
-  connection.connect(function(err) {
-    if (err) { //if database fail connecting
-      console.error('Database connection failed: ' + err.stack);
-      return;
-    } //if database connected
-    console.log('Connected to database.');
-  });
-  const email = user.email;
-  const password = user.pw
-  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-    console.log([email])
-  if (error) {
-    console.log("error ocurred",error);
-  }else{
-    if(results.length >0){
-      if(results[0].password === password){
-        console.log("sucessfull")
-      }else{
-        console.log("password not match")
-      }
-    }else{
-      console.log("email does not exit")
-    }
-  }
-    });
-    connection.end()
-  }
-=======
 connection.connect(function(err) {
     if (err) { //if database fail connecting
         console.error('Database connection failed: ' + err.stack);
@@ -82,7 +28,9 @@ connection.connect(function(err) {
 function register(user) {
     console.log('user data', user)
     const today = new Date();
+    const id = uniqueID();
     const users = {
+        "id":id,
         "first_name": user.first,
         "last_name": user.last,
         "email": user.email,
@@ -125,7 +73,6 @@ function register(user) {
             }
         });
     }
->>>>>>> upstream/master
 
 
     module.exports = {
